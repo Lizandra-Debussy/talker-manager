1fa9ca863ef671f96564265c55fc25d6d459c3e8
Lizandra Debussy <lizandradebussy@hotmail.com>
2022-10-05 08:22:50 -0300

    Requisito 2 concluído

--- src/routers/talkerRouters.js
+++ src/routers/talkerRouters.js
@@ -4,12 +4,20 @@ const router = express.Router();
 const registeredPerson = require('../utils/fs');
 
 router.get('/', async (_req, res) => {
-  try {
-    const response = await registeredPerson();
-    res.status(200).json(response);
-  } catch (err) {
+  const response = await registeredPerson();
+  if (response) {
+    return res.status(200).json(response);
+  } 
     return [];
-  }
+});
+
+router.get('/:id', async (req, res) => {
+    const response = await registeredPerson();
+    const talkerId = response.find(({ id }) => id === Number(req.params.id));
+    if (talkerId) {
+      return res.status(200).json(talkerId);
+    } 
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 module.exports = router;
